fa36af259a7be2cde5b71e44bdeedd62a5eb1de0
Jlestmess <fuscolorenzo07@gmail.com>
2025-11-06 11:53:31 +0100

    Modifiche

--- src/Atleta.java
+++ src/Atleta.java
@@ -13,9 +13,14 @@ public class Atleta implements Runnable {
     }
     @Override
     public void run(){
+        //l'atleta avvisa che partecipa alla gara
         while (metri <= LUNGHEZZAGARA){
-            Scanner metriPercorsi;
-
+            //ogni secondo devi generare un numero random di metri percorsi mertripercorsi
+            Random metripercorsi = new random
+            //e usare sleep
+            //aggiungere questo numero a metri
         }
+        //l'atleta comunica che ha finito di correre
+        //l'atleta verifica se ha vinto (Giudice giudice.verifica())
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
+        //stampa che inizia la gara
         System.out.println("Gara Atletica");
+        //conto alla rovescia
         for (int i = 3; i > 0;i--) {
             System.out.println("inizio in:" +i);
         }
-        System.out.println("Caratteristiche Main"+Thread.currentThread().getName());
+        //System.out.println("Caratteristiche Main"+Thread.currentThread().getName());
+        //istanza giudice
+        //istanza atleti avvio atleti
+        //stampa fine gara
     }
 }
