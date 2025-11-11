e70f058ab0b184797360277eb5b3e965d007e7f1
Jlestmess <fuscolorenzo07@gmail.com>
2025-11-11 14:45:44 +0100

    Modifiche

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class Atleta implements Runnable {
-    int numero;
+     int numero;
     String nome;
-    double tempo = 0;
     double metri = 0;
-    final double LUNGHEZZAGARA = 40000;
+    static final double LUNGHEZZA_GARA = 50.0;
 
-    public Atleta(int pNumero, String pNome){
-        numero = pNumero;
-        nome = pNome;
+    public Atleta(int pNumero, String pNome) {
+        this.numero = pNumero;
+        this.nome = pNome;
+        Giudice.aggiungimi(this);
     }
+
     @Override
-    public void run(){
-        //l'atleta avvisa che partecipa alla gara
-        while (metri <= LUNGHEZZAGARA){
-            //ogni secondo devi generare un numero random di metri percorsi mertripercorsi
-            Random metripercorsi = new random
-            //e usare sleep
-            //aggiungere questo numero a metri
+    public void run() {
+        Random metriPercorsi = new Random();
+        while (metri <= LUNGHEZZA_GARA) {
+            metri += metriPercorsi.nextDouble(10);
+            System.out.println(nome + " Metri Percorsi: " + metri);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.err.println("Errore sleep");
+            }
         }
-        //l'atleta comunica che ha finito di correre
-        //l'atleta verifica se ha vinto (Giudice giudice.verifica())
+        Giudice.finito(this);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
-        //stampa che inizia la gara
         System.out.println("Gara Atletica");
-        //conto alla rovescia
-        for (int i = 3; i > 0;i--) {
-            System.out.println("inizio in:" +i);
-        }
-        //System.out.println("Caratteristiche Main"+Thread.currentThread().getName());
-        //istanza giudice
-        //istanza atleti avvio atleti
-        //stampa fine gara
+
+        new Atleta(67, "Chask");
+        new Atleta(68, "VOLPIIIII!");
+        new Atleta(69, "IL_CUTO");
+
+        Giudice.avviaGara();
     }
 }
